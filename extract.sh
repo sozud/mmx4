5c86441c3a2a6b46a01131ad9f68b0e2293c2df3
sozud <122322823+sozud@users.noreply.github.com>
2024-09-06 20:08:23 -0700

    workflow

--- extract.sh
+++ extract.sh
@@ -1,2 +1,4 @@
-	bchunk "Megaman X4 (USA) [SLUS-00561] Redump/Mega Man X4 (U) [SLUS-00561].bin" "Megaman X4 (USA) [SLUS-00561] Redump/Mega Man X4 (U) [SLUS-00561].cue" disks/mmx4.us.iso
-	7z x disks/mmx4.us.iso01.iso -odisks/us/ || true
+ls disks
+ls disks/dependencies
+bchunk "disks/mmx4.us.bin" "mmx4.us.cue" disks/mmx4.us.iso
+7z x disks/mmx4.us.iso01.iso -odisks/us/ || true
