8047eef77faa429746b858768053442d16145c49
sozud <122322823+sozud@users.noreply.github.com>
2024-09-06 20:20:24 -0700

    workflow

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,4 @@
 wget https://github.com/Xeeynamo/sotn-decomp/releases/download/cc1-psx-26/cc1-psx-26.tar.gz &&
+mkdir -p bin &&
 tar -xvf ./cc1-psx-26.tar.gz -C bin &&
-rm -rf ./cc1-psx-26.tar.*
+rm -rf ./cc1-psx-26.tar.* 
