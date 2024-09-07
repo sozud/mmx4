2b67b8b10a4322f93db40f3bf3202e46718ec7e7
sozud <122322823+sozud@users.noreply.github.com>
2024-09-06 20:08:38 -0700

    workflow

--- extract.sh
+++ extract.sh
@@ -1,4 +1,4 @@
 ls disks
 ls disks/dependencies
-bchunk "disks/mmx4.us.bin" "mmx4.us.cue" disks/mmx4.us.iso
+bchunk "disks/mmx4.us.bin" "disks/mmx4.us.cue" disks/mmx4.us.iso
 7z x disks/mmx4.us.iso01.iso -odisks/us/ || true
