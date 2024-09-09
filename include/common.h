a4100bebbf9107a111fcc0f9a5be84a93e998464
sozud <122322823+sozud@users.noreply.github.com>
2024-09-08 18:09:01 -0700

    Short funcs, funcs_todo tool

--- include/common.h
+++ include/common.h
@@ -19,3 +19,13 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
+
+struct Unk
+{
+    u8 pad[5];
+    u8 unk5;
+    u8 pad6[0x86];
+    u16 unk8C;
+    u32 unk90;
+    u32 unk94;
+};
