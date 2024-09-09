34557a3757b3987765a70a9ed8ecf9d99af5ef3d
sozud <122322823+sozud@users.noreply.github.com>
2024-09-09 00:12:20 -0700

    improve splits (#19)
    * improve splits
    
    * more

---
+++ src/main/2824.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012024);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_8001213C);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800122E0);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012328);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012454);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012560);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012600);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012740);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800127C8);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800127FC);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012854);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800128B8);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800128EC);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012910);
+
+void func_80012934(void) {
+}
+
+void func_8001293C(void) {
+}
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_80012944);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800129A4);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/2824", func_800129F0);
