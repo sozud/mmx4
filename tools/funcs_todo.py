c792c83607098aad585a8b4878ec9aa6760df070
sozud <122322823+sozud@users.noreply.github.com>
2024-09-09 22:16:31 -0700

    Function finder (#25)
    * Enhance functions script
    
    * Enhance functions script
    
    * Enhance functions script
    
    * Enhance functions script

--- tools/funcs_todo.py
+++ tools/funcs_todo.py
@@ -1,4 +1,257 @@
 import os
+import argparse
+from pathlib import Path
+import sys
+from tabulate import tabulate
+import re
+import concurrent.futures
+import json
+import requests
+import sys
+import zipfile
+from io import BytesIO
+import difflib
+
+def are_strings_similar(str1, str2, threshold=0.8):
+    similarity = difflib.SequenceMatcher(None, str1, str2).ratio()
+    return similarity >= threshold
+
+zip_cache = {}
+
+def get_asm(slug):
+    if slug in zip_cache:
+        return zip_cache[slug]
+    url = f'https://decomp.me/api/scratch/{slug}/export'
+    response = requests.get(url)
+    if response.status_code == 200:
+        with zipfile.ZipFile(BytesIO(response.content)) as the_zip:
+            zip_contents = the_zip.namelist()
+            if 'target.s' in zip_contents:
+                with the_zip.open('target.s') as file:
+                    target_content = file.read().decode('utf-8')
+                zip_cache[slug] = target_content
+                return target_content
+            else:
+                print("target.s not found in the zip file")
+    else:
+        print(f"Failed to download the zip file: Status code {response.status_code}")
+    return None
+
+result_cache = {}
+
+def fetch_all_results(url):
+    if url in result_cache:
+        return result_cache[url]
+
+    results = []
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+
+        results.extend(data.get('results', []))
+
+        url = data.get('next')
+
+    result_cache[url] = results
+    return results
+
+def find_scratches(name, platform, local_asm=None, use_local=False):
+    results = fetch_all_results(f"https://decomp.me/api/scratch?search={name}&page_size=100")
+
+    best_result = None
+    best_percent = 0
+
+    for result in results:
+        if not "name" in result:
+            continue
+        if not result["name"].startswith(name):
+            continue
+        if result["platform"] != platform:
+            continue
+
+        if use_local:
+            remote_asm = get_asm(result['slug'])
+
+            if not are_strings_similar(local_asm, remote_asm):
+                continue
+
+        score = result["score"]
+        max_score = result["max_score"]
+        percent = (max_score - score) / max_score
+
+        if percent > best_percent:
+            best_percent = percent
+            best_result = result
+
+    if best_result:
+        return [f"https://decomp.me/scratch/{best_result['slug']}", round(best_percent, 3)]
+
+    return None
+
+parser = argparse.ArgumentParser(
+    description="Create list of undecompiled functions and sort them by difficulty"
+)
+
+parser.add_argument(
+    "--no-fetch",
+    required=False,
+    action="store_true",
+    help="Disable fetching scratch links from decomp.me",
+)
+
+parser.add_argument(
+    "--keywords",
+    metavar="keyword",
+    type=str,
+    nargs="+",
+    help="Only match paths with these keywords",
+)
+
+# look in asm files, read in the text and check for branches and jump tables
+def get_asm_files(asm_path, og_files):
+    files = []
+    for path in Path(asm_path).rglob("*.s"):
+        found = False
+        for file, size in og_files:
+            if Path(file) == path:
+                found = True
+        if not found:
+            continue
+        # ignore data
+        if not "nonmatchings" in str(path):
+            continue
+        f = open(f"{path}", "r")
+        text = f.read()
+
+        # check for different mips branch types and count
+        branches = 0
+        branch_types = [
+            "b          ",
+            "bczt       ",
+            "bczf       ",
+            "beq        ",
+            "beqz       ",
+            "bge        ",
+            "bgeu       ",
+            "bgez       ",
+            "bgezal     ",
+            "bgt        ",
+            "bgtu       ",
+            "bgtz       ",
+            "ble        ",
+            "bleu       ",
+            "blez       ",
+            "bgezal     ",
+            "bltzal     ",
+            "blt        ",
+            "bltu       ",
+            "bltz       ",
+            "bne        ",
+            "bnez       ",
+            "j          ",
+            "jal        ",
+            "jalr       ",
+            "jr         ",
+        ]
+        for branch in branch_types:
+            branches = branches + text.count(branch)
+
+        jump_table = "   "
+        if "jpt_" in text or "jtbl_" in text:
+            jump_table = "Yes"
+
+        f = {"name": path, "text": text, "branches": branches, "jump_table": jump_table}
+
+        files.append(f)
+
+    return files
+
+def find_wip(o):
+    result = find_scratches(o[1], "ps1", o[7], True)
+
+    if result:
+        return {"link": result[0], "percent": result[1]}
+
+    return None
+
+
+def do_files(files):
+    args = parser.parse_args()
+    asm_files = get_asm_files("asm/us", files)
+
+    # sort by name, then number of branches, then length
+    asm_files = sorted(asm_files, key=lambda x: (x["name"]))
+    asm_files = sorted(asm_files, key=lambda x: (x["branches"]))
+    asm_files = sorted(asm_files, key=lambda x: len(x["text"].split("\n")))
+
+    if args.keywords and len(args.keywords):
+        # filter based on keywords
+        asm_files = [
+            file
+            for file in asm_files
+            if any(keyword in str(file["name"]) for keyword in args.keywords)
+        ]
+
+    output = []
+
+    for f in asm_files:
+        name = str(f["name"])
+        length = len(f["text"].split("\n"))
+        branches = f["branches"]
+        jump_table = f["jump_table"]
+
+        if "/psxsdk/" in name:
+            ovl_name = name.split("/")[5]  # grab library name
+            func_name = os.path.splitext(os.path.basename(name))[0]
+        elif "/weapon/" in name:
+            # use the weapon name
+            ovl_name = name.split("/")[4]  # grab library name
+            func_name = os.path.splitext(os.path.basename(name))[0]
+        else:
+            matches = re.search(r"\/(\w+)\/nonmatchings\/\w+\/(\w+)\.s", name)
+            if matches:
+                ovl_name = matches.group(1)
+                func_name = matches.group(2)
+            else:
+                ovl_name = ""
+                func_name = name
+
+        wip = ""
+        wip_percentage = ""
+        output.append(
+            [
+                ovl_name,
+                func_name,
+                length,
+                branches,
+                jump_table,
+                wip,
+                wip_percentage,
+                f["text"] # local asm
+            ]
+        )
+
+    if not args.no_fetch:
+        # we are mostly waiting on IO so run in parallel
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(find_wip, o) for o in output]
+            results = [f.result() for f in futures]
+
+        # Update output with the results
+        for i, o in enumerate(output):
+            # keep the in-source results as definitive
+            if results[i] != None:
+                o[5] = results[i]["link"]
+                o[6] = results[i]["percent"]
+
+    # delete asm text
+    for o in output:
+        del o[7:]
+
+    headers = ["Ovl", "Function", "Length", "Branches", "Jtbl", "WIP", "%"]
+    print(tabulate(output, headers=headers, tablefmt="github"))
+
 
 # Function to get the path of the file in the same directory as the script
 def get_file_path(filename):
@@ -36,13 +289,14 @@ def main():
     # Find matching files and sort them by size
     matching_files = find_matching_files_sorted_by_size('asm', hex_numbers)
 
-    # Print matching files with their sizes
-    if matching_files:
-        print(f"Matching files found ({len(matching_files)}), sorted by size:")
-        for file, size in matching_files:
-            print(f"{file} - {size} bytes")
-    else:
-        print("No matching files found.")
+    do_files(matching_files)
+    # # Print matching files with their sizes
+    # if matching_files:
+    #     print(f"Matching files found ({len(matching_files)}), sorted by size:")
+    #     for file, size in matching_files:
+    #         print(f"{file} - {size} bytes")
+    # else:
+    #     print("No matching files found.")
 
 if __name__ == "__main__":
     main()
