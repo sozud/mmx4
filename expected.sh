c21d0137f09877bb700da6f1dc97ab1b066e08da
Josh Lory <josh.lory@outlook.com>
2024-10-22 04:59:30 +0000

    Enable asm differ

--- expected.sh
+++ expected.sh
@@ -2,3 +2,5 @@ mkdir -p expected/us
 cp build/us/main.o expected/us/main.o
 cp build/us/main.bin expected/us/main.bin
 cp build/us/main.map expected/us/main.map
+mkdir -p expected/build/us
+cp -r build/us/src expected/build/us/src
