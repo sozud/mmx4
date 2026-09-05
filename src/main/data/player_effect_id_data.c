e94f06dae43a560b5f7afef2a6aa990c6ae587ba
sozud <122322823+sozud@users.noreply.github.com>
2026-09-05 04:10:42 +0000

    fix

--- src/main/data/player_effect_id_data.c
+++ src/main/data/player_effect_id_data.c
@@ -3,3 +3,7 @@
 u8 D_800F8B30[2] = { 0x67, 0x66 };
 
 static u16 s_PlayerEffectIdPad __attribute__((section(".data"))) = 0;
+
+u8 D_800F8B34[8] = { 2, 3, 6, 6, 2, 4, 7, 7 };
+
+s16 D_800F8B3C[4] = { 0x780D, 0x780E, 0x780F, 0 };
