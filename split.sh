92e05ec596e08cc31e19d1b4c2450b215a368e8c
prj-2501 <project_2501@emailuser.net>
2026-09-19 17:20:58 +0530

    split.sh: obey VERSION

--- split.sh
+++ split.sh
@@ -1,1 +1,7 @@
-rm -rf asm && rm -rf assets && splat split ./config/SLUS_005.61.splat.yaml 
+#!/bin/sh
+if [ "${VERSION:-us}" = "us" ]; then
+	SPLAT_CONFIG=SLUS_005.61.splat.yaml
+else # jp
+	SPLAT_CONFIG=SLPS_009.02.splat.yaml
+fi
+rm -rf asm && rm -rf assets && splat split ./config/${SPLAT_CONFIG}
