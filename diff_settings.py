42f4e189686862fcf14a3734c6bc759bef075f2d
prj-2501 <project_2501@emailuser.net>
2026-09-21 16:30:15 +0530

    diff_settings: added "expected_dir","build_dir",.. config fields

--- diff_settings.py
+++ diff_settings.py
@@ -3,3 +3,9 @@ def apply(config, args):
     config["myimg"] = "build/us/main.bin"
     config["mapfile"] = "build/us/main.map"
     config["objdump_executable"] = "mipsel-linux-gnu-objdump"
+    config["source_directories"] = ["src", "include"]
+    config["show_line_numbers_default"] = True
+    config["arch"] = "mipsel"
+    config["map_format"] = "gnu"
+    config["build_dir"] = "build/us"
+    config["expected_dir"] = "expected/us"
